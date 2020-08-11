0ed65cd23ae9dba66f6b5fb1915301e98e868d26
Fillipe <fillipe484@gmail.com>
2020-08-11 20:23:19 -0300

    How to use native modules

--- modules/index.js
+++ modules/index.js
@@ -1,3 +1,8 @@
 const {printName, lastName} = require ('./printName');
 
 printName(`Fillipe Augusto ${lastName}`); 
+
+
+// Using native modules 
+const os = require('os');
+console.log(os.type());
