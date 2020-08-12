2691730be5fba511d1cae4e3e6c9088fca026b2d
Fillipe <fillipe484@gmail.com>
2020-08-11 21:11:45 -0300

    creating user Controller and Routes sys

---
+++ firstapi/src/controllers/UserController.js
@@ -0,0 +1,9 @@
+/* == Create Read Update Delete (CRUD) == */
+const users = require('../mocks/users');
+
+module.exports = {
+  listUsers(request,response){
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+  },
+};
