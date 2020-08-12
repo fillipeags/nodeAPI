6126d47d8fbcdccf1797903242eee0fe98e8a0a5
Fillipe <fillipe484@gmail.com>
2020-08-12 10:34:24 -0300

    DELETE method | deleteUser function

--- firstapi/src/routes.js
+++ firstapi/src/routes.js
@@ -24,4 +24,10 @@ module.exports = [
     method: 'PUT',
     handler: UserController.updateUser,
   },
+
+  {
+    endpoint: '/users/:id',
+    method: 'DELETE',
+    handler: UserController.deleteUser,
+  },
 ];
