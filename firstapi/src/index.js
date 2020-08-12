0853b90242ee2126fa6bb4efcd9697c9a58234f6
Fillipe <fillipe484@gmail.com>
2020-08-12 10:13:39 -0300

    helper bodyParser

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const url = require('url');
 
+const bodyParser = require('./helpers/bodyParser');
 const routes = require('./routes');
+
 const { parse } = require('path');
 
 const server = http.createServer((request,response) => {
@@ -32,7 +34,12 @@ const server = http.createServer((request,response) => {
       response.end(JSON.stringify(body));
     };
 
-    route.handler(request,response);
+    if (['POST', 'PUT', 'PATCH'].includes(request.method)){
+      bodyParser(request, () => route.handler(request,response));  
+    }else{
+      route.handler(request,response);      
+    }
+
   }else{
     response.writeHead(404, {'Content-Type': 'text/html'});
     response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
