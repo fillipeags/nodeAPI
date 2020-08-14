8af73fd188a98a2f642a3ef773d739ded09a649d
Fillipe <fillipe484@gmail.com>
2020-08-14 20:57:27 -0300

    Put Route | editing Contacts

--- mycontacts/src/App/controllers/ContactController.js
+++ mycontacts/src/App/controllers/ContactController.js
@@ -41,8 +41,33 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
-    // Editar um registro
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This email has already been taken' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
