f77941018a6c97036296f96bfe60462bce084ef5
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2020-10-28 18:57:34 -0300

    empty repo

--- modules/index.js
+++
@@ -1,8 +0,0 @@
-const {printName, lastName} = require ('./printName');
-
-printName(`Fillipe Augusto ${lastName}`); 
-
-
-// Using native modules 
-const os = require('os');
-console.log(os.type());
