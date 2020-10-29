6d4de9387f373a10d061a3c88686716fbad2156b
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2020-10-29 16:32:12 -0300

    response.send method

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -24,8 +24,13 @@ const server = http.createServer((request,response) => {
   ));
 
   if (route){
-    request.query = parsedUrl.query
+    request.query = parsedUrl.query;
     request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(body));
+    }
 
     route.handler(request,response);
   }else{
