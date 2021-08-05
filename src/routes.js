c7f5ff03ed08c19be2513209551fbabba73ac477
Fillipe <fillipe484@gmail.com>
2021-08-05 00:01:29 -0300

    chore: update readme

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,31 @@
 const UserController = require('./controllers/UserController')
 
+
 module.exports = [
   {
     endpoint: '/users',
     method: 'GET',
-    handler:  UserController.listUsers,
+    handler: UserController.listUsers,
   },
   {
     endpoint: '/users/:id',
     method: 'GET',
-    handler:  UserController.getUserById,
+    handler: UserController.getUserById,
   },
   {
     endpoint: '/users',
     method: 'POST',
-    handler:  UserController.createUser,
+    handler: UserController.createUser,
   },
   {
     endpoint: '/users/:id',
     method: 'PUT',
-    handler:  UserController.updateUser,
+    handler: UserController.updateUser,
   },
 
   {
     endpoint: '/users/:id',
     method: 'DELETE',
-    handler:  UserController.deleteUser,
+    handler: UserController.deleteUser,
   },
 ];
